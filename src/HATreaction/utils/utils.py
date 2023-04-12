48c9d8b9ca4645b148648b315ed54ac9263fda4e
KRiedmiller <kai.riedmiller@web.de>
2023-04-12 10:57:51 +0200

    Tests (#3)
    * capping tests inputs
    
    * Test for capping using saved reference
    
    * pin numpy version as kgcnn requires <=1.23

--- src/HATreaction/utils/utils.py
+++ src/HATreaction/utils/utils.py
@@ -9,7 +9,7 @@ def find_radicals(u):
         ('H','HW','HO','HS','HA','HC','H1','H2','H3','HP','H4','H5','HO','H0','HP','O','O2','Cl','Na','I','F','Br'):1,
         ('NB','NC','OW','OH','OS','SH','S'):2,
         ('C','CN','CB','CR','CK','CC','CW','CV','C*','CQ','CM','CA','CD','CZ','N','NA','N*','N2'):3,
-        ('CT','N3','P','SO'):4}                                               #compare to atom type perception paper (2006) same as in changemanager.py
+        ('CT','N3','P','SO'):4}     #compare to atom type perception paper (2006) same as in changemanager.py
     atoms = []
     for atom in u.atoms:
         if atom.resname == 'SOL':
