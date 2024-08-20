59b4befa07e368195c71a92acd454c0b4187f8d8
KRiedmiller <kai.riedmiller@web.de>
2024-08-20 16:12:23 +0200

    fix

--- src/HATreaction/utils/trajectory_utils.py
+++ src/HATreaction/utils/trajectory_utils.py
@@ -16,11 +16,7 @@ from HATreaction.utils.utils import check_cylinderclash
 from typing import Optional
 import numpy.typing as npt
 from pathlib import Path
-<<<<<<< HEAD
 from tqdm.autonotebook import tqdm
-
-=======
->>>>>>> bb3c1eb (remove whitespace)
 
 version = 0.9
 
