6d19872219cedab0a0b26018b6fe31acaa387949
Glen Hunter <29704413+ghhunter@users.noreply.github.com>
2025-08-28 13:50:01 +0200

    feat: integration tests for restart

--- tests/test_hat_integration.py
+++ tests/test_hat_integration.py
@@ -31,5 +31,39 @@ def test_integration_hat_reaction(arranged_tmp_path):
         Path("alanine_hat_000.kimmdy.log")
     )
     assert (
-        len(list(Path.cwd().glob("alanine_hat_000/*"))) == 22
+        len(list(Path.cwd().glob("alanine_hat_000/*"))) == 14
     )  # don't forget, .kimmdy_finished counts
+
+@pytest.mark.parametrize("arranged_tmp_path", (["test_hat_integration"]), indirect=True)
+def test_integration_hat_restart(arranged_tmp_path):
+    run_dir = Path("alanine_hat_000")
+    kimmdy_run(input=Path("kimmdy_restart.yml"))
+    n_files_original = len(list(run_dir.glob("*")))
+
+    # restart already finished run
+    kimmdy_run(input=Path("kimmdy_restart.yml"))
+    assert "already finished" in read_last_line(Path("alanine_hat_000.kimmdy.log"))
+
+    # try restart from stopped md
+    task_dirs = get_task_directories(run_dir)
+    (task_dirs[-1] / MARK_DONE).unlink()
+    (arranged_tmp_path / run_dir / MARK_FINISHED).unlink()
+    kimmdy_run(input=Path("kimmdy_restart.yml"))
+    n_files_continue_md = len(list(run_dir.glob("*"))) 
+
+    assert "Finished running last task" in read_last_line(
+        Path("alanine_hat_000.kimmdy.log")
+    )
+    assert n_files_original == n_files_continue_md == 15
+
+    # try restart from finished md
+    task_dirs = get_task_directories(run_dir)
+    (task_dirs[-4] / MARK_DONE).unlink()
+    (arranged_tmp_path / run_dir / MARK_FINISHED).unlink()
+    kimmdy_run(input=Path("kimmdy_restart.yml"))
+    n_files_restart = len(list(run_dir.glob("*")))
+
+    assert "Finished running last task" in  read_last_line(
+        Path("alanine_hat_000.kimmdy.log")
+    )
+    assert n_files_original == n_files_restart == 15
