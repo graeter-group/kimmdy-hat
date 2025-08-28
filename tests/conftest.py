31a305e18e00a7c0c358090e4c934614e552f934
Glen Hunter <29704413+ghhunter@users.noreply.github.com>
2025-08-28 10:12:48 +0200

    feat: Integration test for plugin with KIMMDY (#42)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,50 @@
+"""File for pytest configuration in python and fixture definition.
+
+The name 'conftest.py' is recognized by pytest to execute it before tests.
+"""
+
 import pytest
+import shutil
+import os
+from pathlib import Path
+from dataclasses import dataclass
+from typing import Callable
+from kimmdy.plugins import discover_plugins
+
+from kimmdy.tasks import TaskFiles
+from kimmdy.utils import get_gmx_dir
 
 
+## fixtures for setup and teardown ##
+@pytest.fixture
+def arranged_tmp_path(tmp_path: Path, request: pytest.FixtureRequest):
+    """Arrange temporary directory for tests.
+
+    With files for the test and a symlink to forcefield.
+    """
+
+    # if fixture was parameterized, use this for directory with input files
+    if hasattr(request, "param"):
+        file_dir = Path(__file__).parent / request.param
+    # else use stem of requesting file to find directory with input files
+    else:
+        file_dir = Path(__file__).parent / request.path.stem
+    # arrange tmp_path
+
+    shutil.copytree(file_dir, tmp_path, dirs_exist_ok=True)
+    assetsdir = Path(__file__).parent / "assets"
+
+    if not (tmp_path / "amber99sb-star-ildnp.ff").exists():
+        Path(tmp_path / "amber99sb-star-ildnp.ff").symlink_to(
+            assetsdir / "amber99sb-star-ildnp.ff",
+            target_is_directory=True,
+        )
+    # change cwd to tmp_path
+    os.chdir(tmp_path.resolve())
+    return tmp_path
+
+
+# Functions for parsing --gpu to enable gpu teest
 def pytest_addoption(parser):
     parser.addoption(
         "--gpu",
