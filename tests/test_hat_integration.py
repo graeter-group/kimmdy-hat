8e589d4df707006044689642a83fa5fd4ef85d53
Kai Riedmiller <kai.riedmiller@iwr.uni-heidelberg.de>
2025-08-29 16:27:54 +0200

    chore: cleanup

--- tests/test_hat_integration.py
+++ tests/test_hat_integration.py
@@ -1,14 +1,8 @@
 import os
-import subprocess as sp
 from pathlib import Path
-
-import pytest
 
 from kimmdy.cmd import kimmdy_run
 from kimmdy.constants import MARK_DONE, MARK_FINISHED
-from kimmdy.parsing import read_top, write_top
-from kimmdy.plugins import parameterization_plugins
-from kimmdy.topology.topology import Topology
 from kimmdy.utils import get_task_directories
 
 
@@ -23,9 +17,7 @@ def read_last_line(file):
         return f.readline().decode()
 
 
-@pytest.mark.parametrize("arranged_tmp_path", (["test_hat_integration"]), indirect=True)
 def test_integration_hat_reaction(arranged_tmp_path):
-
     kimmdy_run()
     assert "Finished running last task" in read_last_line(
         Path("alanine_hat_000.kimmdy.log")
@@ -35,7 +27,6 @@ def test_integration_hat_reaction(arranged_tmp_path):
     )  # don't forget, .kimmdy_finished counts
 
 
-@pytest.mark.parametrize("arranged_tmp_path", (["test_hat_integration"]), indirect=True)
 def test_integration_hat_restart(arranged_tmp_path):
     run_dir = Path("alanine_hat_000")
     kimmdy_run(input=Path("kimmdy_restart.yml"))
